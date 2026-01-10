da3465789293dde4aa6886ec0564c2d43e9e603f
Purnika Khanal <khanalpurnika@gmail.com>
2026-01-10 12:24:34 +0545

    1st commit

---
+++ DecisionTreeClassifier.py
@@ -0,0 +1,7 @@
+from sklearn import tree
+x = [[180], [183], [179], [165], [170], [160], [182], [155], [177], [178], [176]]
+y = ['male', 'male', 'female', 'female', 'female', 'female', 'male', 'female', 'male', 'female', 'female']
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(x, y)
+prediction = clf.predict([[168]])
+print(prediction)
